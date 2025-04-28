b4d08e0e43cef6aa004800073dbc432c1758160d
ViiesTech <viistech.developer@gmail.com>
2025-04-29 04:11:55 +0500

    Implement Favourites flow UI completed

--- src/components/SaloonsCard.tsx
+++ src/components/SaloonsCard.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-native/no-inline-styles */
-import {View, Text, Image, TouchableOpacity} from 'react-native';
-import React from 'react';
+import {View, Text, Image, TouchableOpacity, Animated} from 'react-native';
+import React, {useEffect, useRef} from 'react';
 import {
   responsiveFontSize,
   responsiveHeight,
@@ -10,7 +10,9 @@ import AppText from './AppTextComps/AppText';
 import AppColors from '../utils/AppColors';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Entypo from 'react-native-vector-icons/Entypo';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 import {useNavigation} from '@react-navigation/native';
+
 type props = {
   img?: any;
   title?: string;
@@ -18,6 +20,12 @@ type props = {
   KM?: string;
   Rating?: number;
   TotalNoOfRating?: number;
+  component?: any;
+  isShowDeleteIcon?: any;
+  setIsShowDeleteIcon?: any;
+  itemId?: any;
+  onPress?: any;
+  setShowRemoveModal?:any;
 };
 
 const SaloonsCard = ({
@@ -27,73 +35,131 @@ const SaloonsCard = ({
   img,
   location,
   title,
+  component,
+  setIsShowDeleteIcon,
+  itemId,
+  isShowDeleteIcon,
+  setShowRemoveModal,
 }: props) => {
   const navigation = useNavigation();
+  const translateX = useRef(new Animated.Value(0)).current;
+  const isOpen = isShowDeleteIcon?.id === itemId;
+
+  const handlePress = () => {
+    if (!isOpen) {
+      setIsShowDeleteIcon({id: itemId, shown: true});
+    } else {
+      setIsShowDeleteIcon({id: 0, shown: false});
+    }
+  };
+
+  useEffect(() => {
+    Animated.timing(translateX, {
+      toValue: isOpen ? -80 : 0,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  }, [isOpen]);
+
   return (
-    <TouchableOpacity onPress={() => navigation.navigate('HomeDetails')}>
-      <View
-        style={{
-          padding: 20,
-          borderRadius: 10,
-          width: responsiveWidth(90),
-          alignSelf: 'center',
-          flexDirection: 'row',
-          alignItems: 'center',
-          backgroundColor: AppColors.WHITE,
+    <Animated.View style={component ? {transform: [{translateX}]} : {}}>
+      <TouchableOpacity
+        onPress={() => {
+          if (component) {
+            handlePress();
+          } else {
+            navigation.navigate('HomeDetails');
+          }
         }}>
-        <Image
-          source={img}
-          style={{
-            height: responsiveHeight(10),
-            width: responsiveHeight(10),
-            resizeMode: 'contain',
-            borderRadius: 10,
-            marginRight: 10,
-          }}
-        />
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            width: responsiveWidth(60),
-          }}>
-          <View style={{gap: 5}}>
-            <AppText
-              title={title}
-              textColor={AppColors.BLACK}
-              textSize={2.5}
-              textFontWeight
+        <View style={{flexDirection: 'row'}}>
+          <View
+            style={{
+              padding: 20,
+              borderRadius: 10,
+              width: responsiveWidth(90),
+              alignSelf: 'center',
+              flexDirection: 'row',
+              alignItems: 'center',
+              backgroundColor: AppColors.WHITE,
+            }}>
+            <Image
+              source={img}
+              style={{
+                height: responsiveHeight(10),
+                width: responsiveHeight(10),
+                resizeMode: 'contain',
+                borderRadius: 10,
+                marginRight: 10,
+              }}
             />
-            <View style={{flexDirection: 'row', alignItems: 'center', gap: 5}}>
-              <Ionicons
-                name={'location-outline'}
-                size={responsiveFontSize(2)}
-                color={AppColors.DARKGRAY}
-              />
-              <AppText
-                title={location}
-                textSize={2}
-                textColor={AppColors.DARKGRAY}
-              />
-            </View>
-            <View style={{flexDirection: 'row', alignItems: 'center', gap: 5}}>
-              <Entypo
-                name={'star'}
-                size={responsiveFontSize(2.5)}
-                color={'#FFD33C'}
-              />
-              <AppText title={Rating} textSize={2} textFontWeight />
-              <AppText
-                title={`(${TotalNoOfRating})`}
-                textSize={1.5}
-                textColor={AppColors.DARKGRAY}
-              />
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                width: responsiveWidth(60),
+              }}>
+              <View style={{gap: 5}}>
+                <AppText
+                  title={title}
+                  textColor={AppColors.BLACK}
+                  textSize={2.5}
+                  textFontWeight
+                />
+                <View
+                  style={{flexDirection: 'row', alignItems: 'center', gap: 5}}>
+                  <Ionicons
+                    name={'location-outline'}
+                    size={responsiveFontSize(2)}
+                    color={AppColors.DARKGRAY}
+                  />
+                  <AppText
+                    title={location}
+                    textSize={2}
+                    textColor={AppColors.DARKGRAY}
+                  />
+                </View>
+                <View
+                  style={{flexDirection: 'row', alignItems: 'center', gap: 5}}>
+                  <Entypo
+                    name={'star'}
+                    size={responsiveFontSize(2.5)}
+                    color={'#FFD33C'}
+                  />
+                  <AppText title={Rating} textSize={2} textFontWeight />
+                  <AppText
+                    title={`(${TotalNoOfRating})`}
+                    textSize={1.5}
+                    textColor={AppColors.DARKGRAY}
+                  />
+                </View>
+              </View>
             </View>
           </View>
-          <AppText title={`${KM} KM`} textColor={AppColors.DARKGRAY} />
+          <TouchableOpacity
+            onPress={() => setShowRemoveModal(true)}
+            style={{
+              backgroundColor: '#FA52521A',
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginLeft: responsiveWidth(2),
+              paddingHorizontal: responsiveWidth(2.7),
+              borderRadius: 10,
+              gap: 5,
+            }}>
+            <AntDesign
+              name={'delete'}
+              size={responsiveFontSize(2.5)}
+              color={AppColors.BTNCOLOURS}
+            />
+            <AppText
+              title="Remove"
+              textColor={AppColors.BTNCOLOURS}
+              textSize={1.7}
+            />
+          </TouchableOpacity>
         </View>
-      </View>
-    </TouchableOpacity>
+      </TouchableOpacity>
+    </Animated.View>
   );
 };
 
--- src/screens/main/favourites/Favourites.jsx
+++ src/screens/main/favourites/Favourites.jsx
@@ -1,12 +1,63 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, {useState} from 'react';
+import {View, ScrollView, FlatList} from 'react-native';
+import AppHeader from '../../../components/AppHeader';
+import {useNavigation} from '@react-navigation/native';
+import AppColors from '../../../utils/AppColors';
+import LineBreak from '../../../components/LineBreak';
+import {responsiveWidth} from '../../../utils/Responsive_Dimensions';
+import SaloonsArray from '../../../utils/SaloonsArray';
+import SaloonsCard from '../../../components/SaloonsCard';
+import RemoveFavouritesModal from '../../../components/RemoveFavouritesModal';
 
 const Favourites = () => {
-  return (
-    <View>
-      <Text>Favourites</Text>
-    </View>
-  )
-}
+  const navigation = useNavigation();
+  const [isShowDeleteIcon, setIsShowDeleteIcon] = useState({
+    id: 0,
+    shown: false,
+  });
+  const [showRemoveModal, setShowRemoveModal] = useState(false);
 
-export default Favourites
+  return (
+    <ScrollView style={{flex: 1, backgroundColor: AppColors.APPBG}}>
+      <AppHeader onPress={() => navigation.goBack()} title="Favourites" />
+      <LineBreak space={2} />
+
+      <RemoveFavouritesModal
+        visible={showRemoveModal}
+        handleAppointmentButtonPress={() => {
+          setShowRemoveModal(false);
+          setIsShowDeleteIcon({id: 0, shown: false});
+        }}
+        handleCancelButtonPress={() => {
+          setShowRemoveModal(false);
+          setIsShowDeleteIcon({id: 0, shown: false});
+        }}
+      />
+
+      <View style={{paddingHorizontal: responsiveWidth(5)}}>
+        <FlatList
+          data={SaloonsArray}
+          contentContainerStyle={{gap: 10}}
+          keyExtractor={item => item.id.toString()}
+          renderItem={({item}) => (
+            <SaloonsCard
+              title={item.title}
+              KM={item.KM}
+              Rating={item.Rating}
+              TotalNoOfRating={item.TotalNoOfRating}
+              img={item.img}
+              location={item.location}
+              component="fav"
+              itemId={item.id}
+              isShowDeleteIcon={isShowDeleteIcon}
+              setIsShowDeleteIcon={setIsShowDeleteIcon}
+              setShowRemoveModal={setShowRemoveModal}
+            />
+          )}
+        />
+      </View>
+    </ScrollView>
+  );
+};
+
+export default Favourites;
