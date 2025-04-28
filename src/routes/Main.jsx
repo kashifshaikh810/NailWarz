80bb544de05ff897700266140ab4adcb7111ee14
ViiesTech <viistech.developer@gmail.com>
2025-04-28 23:50:20 +0500

    Bottom tabs icons setup

--- src/routes/Main.jsx
+++ src/routes/Main.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
+import {Image} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Home from '../screens/main/Home';
 import HomeDetails from '../screens/main/HomeDetails';
 import StylistSelect from '../screens/main/StylistSelect';
@@ -10,6 +12,17 @@ import SelectPaymentMethod from '../screens/main/SelectPaymentMethod';
 import DownloadReceipt from '../screens/main/DownloadReceipt';
 import SearchLocation from '../screens/main/SearchLocation';
 import MapView from '../screens/main/MapView';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Profile from '../screens/main/profile/Profile';
+import Warz from '../screens/main/warz/Warz';
+import Favourites from '../screens/main/favourites/Favourites';
+import Booking from '../screens/main/bookings/Booking';
+import {
+  responsiveFontSize,
+  responsiveHeight,
+  responsiveWidth,
+} from '../utils/Responsive_Dimensions';
+import APPImages from '../assets/APPImages';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
@@ -20,7 +33,10 @@ const Main = () => {
       <Stack.Screen name="Home" component={MyTabs} />
       <Tab.Screen name="HomeDetails" component={HomeDetails} />
       <Tab.Screen name="StylistSelect" component={StylistSelect} />
-      <Tab.Screen name="DateAndTimeSelection" component={DateAndTimeSelection} />
+      <Tab.Screen
+        name="DateAndTimeSelection"
+        component={DateAndTimeSelection}
+      />
       <Tab.Screen name="BookingSummary" component={BookingSummary} />
       <Tab.Screen name="SelectPaymentMethod" component={SelectPaymentMethod} />
       <Tab.Screen name="DownloadReceipt" component={DownloadReceipt} />
@@ -33,9 +49,41 @@ const Main = () => {
 
 function MyTabs() {
   return (
-    <Tab.Navigator screenOptions={{headerShown:false}}>
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        headerShown: false,
+        tabBarLabelStyle: {fontSize: responsiveFontSize(1.7)},
+        tabBarStyle: {height: responsiveHeight(8)},
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Profile') {
+            iconName = focused ? 'person' : 'person-outline';
+          } else if (route.name === 'Booking') {
+            iconName = focused ? 'calendar' : 'calendar-outline';
+          } else if (route.name === 'Favourites') {
+            iconName = focused ? 'heart' : 'heart-outline';
+          }
+
+          if (route.name === 'Warz') {
+            return (
+              <Image
+                source={APPImages.LOGO}
+                style={{width: responsiveWidth(6.5), height: responsiveHeight(3.5)}}
+              />
+            );
+          } else {
+            return <Ionicons name={iconName} size={size} color={color} />;
+          }
+        },
+      })}>
       <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Profile" component={Home} />
+      <Tab.Screen name="Booking" component={Booking} />
+      <Tab.Screen name="Warz" component={Warz} />
+      <Tab.Screen name="Favourites" component={Favourites} />
+      <Tab.Screen name="Profile" component={Profile} />
     </Tab.Navigator>
   );
 }
---
+++ src/screens/main/warz/Warz.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Warz = () => {
+  return (
+    <View>
+      <Text>Warz</Text>
+    </View>
+  )
+}
+
+export default Warz
